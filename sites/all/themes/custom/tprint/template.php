fc75ddcc3a926826103b6218ae37172936e15919
= <tkuldeep.allen@gmail.com>
2014-07-23 19:42:42 +0530

    Description: Add form for configuring slider in search form. Steps: Clear the all cache.

--- sites/all/themes/custom/tprint/template.php
+++ sites/all/themes/custom/tprint/template.php
@@ -4,3 +4,24 @@
  * @file
  * Theme template customizations.
  */
+
+/**
+ * Implements template_preprocess_page().
+ */
+function tprint_preprocess_page(&$variables) {
+
+  // Set background image product search form
+  if ($variables['is_front']) {
+    $product_classes = uc_product_class_load();
+    foreach ($product_classes as $key => $value) {
+      $fid = variable_get('slider_image_' . $key, NULL);
+      $bg_image = file_load($fid);
+      if ($bg_image != '' || $bg_image != NULL) {
+        $variables['page']['search_bg'][] = file_create_url($bg_image->uri);
+      }
+      else {
+        $variables['page']['search_bg'][] = '';
+      }
+    }
+  }
+}
